06e11418643d03d3bd58055b86790b088c4ba469
doppleddiggong <doppleddiggong@users.noreply.github.com>
2025-10-06 06:58:07 +0000

    deploy: c1ec2a14bb86cc83952242eb93dc603c6a86f69c

--- classFScreenshotFeature.js
+++ classFScreenshotFeature.js
@@ -3,9 +3,5 @@ var classFScreenshotFeature =
     [ "FScreenshotFeature", "classFScreenshotFeature.html#a8563baa30252566aeabe473b66be094f", null ],
     [ "CaptureActiveViewport", "classFScreenshotFeature.html#aaab75697de4685c906b26140e54301b6", null ],
     [ "GenerateScreenshotMenu", "classFScreenshotFeature.html#a56fc11b5f6023d5492e26d9354807082", null ],
-    [ "OnCaptureHighResolution", "classFScreenshotFeature.html#a31ee01aa337c4b724c6ec8a550f99c05", null ],
-    [ "OnCaptureQuickPreview", "classFScreenshotFeature.html#a7189e1c261246eb0d4f1a2af11ab89bd", null ],
-    [ "OnCaptureStandard", "classFScreenshotFeature.html#afc1b23933c9d67077c2e726610688605", null ],
-    [ "OnCaptureUltra", "classFScreenshotFeature.html#a756ceabcb0e485733fa095066f2970fa", null ],
     [ "OnOpenScreenShotDir", "classFScreenshotFeature.html#af01017cd01130c08bf1c63309ea1e2a6", null ]
 ];
--- search/all_f.js
+++ search/all_f.js
@@ -1,13 +1,9 @@
 var searchData=
 [
-  ['oncapturehighresolution_0',['OnCaptureHighResolution',['../classFScreenshotFeature.html#a31ee01aa337c4b724c6ec8a550f99c05',1,'FScreenshotFeature']]],
-  ['oncapturequickpreview_1',['OnCaptureQuickPreview',['../classFScreenshotFeature.html#a7189e1c261246eb0d4f1a2af11ab89bd',1,'FScreenshotFeature']]],
-  ['oncapturestandard_2',['OnCaptureStandard',['../classFScreenshotFeature.html#afc1b23933c9d67077c2e726610688605',1,'FScreenshotFeature']]],
-  ['oncaptureultra_3',['OnCaptureUltra',['../classFScreenshotFeature.html#a756ceabcb0e485733fa095066f2970fa',1,'FScreenshotFeature']]],
-  ['onexecutebuttoncommand_4',['OnExecuteButtonCommand',['../classFCommandFeature.html#a3ff8a0398db30ce422496f2281dd3b60',1,'FCommandFeature']]],
-  ['onopenscreenshotdir_5',['OnOpenScreenShotDir',['../classFScreenshotFeature.html#af01017cd01130c08bf1c63309ea1e2a6',1,'FScreenshotFeature']]],
-  ['onselectedmap_6',['OnSelectedMap',['../classFLevelFeature.html#ac20da5828b1adc7686fbd96ba8abd1c8',1,'FLevelFeature']]],
-  ['onselectedmap_5fopen_7',['OnSelectedMap_Open',['../classFLevelFeature.html#a536602356f4e9dcdfcfbe85094782bdb',1,'FLevelFeature']]],
-  ['onselectedmap_5fplayineditor_8',['OnSelectedMap_PlayInEditor',['../classFLevelFeature.html#a31e8a2482aafbc4082a996e3a5a7a0d9',1,'FLevelFeature']]],
-  ['onselectedmap_5fplayselectedviewport_9',['OnSelectedMap_PlaySelectedViewport',['../classFLevelFeature.html#ad659ce46cd4d37948b219e9aa104c2b3',1,'FLevelFeature']]]
+  ['onexecutebuttoncommand_0',['OnExecuteButtonCommand',['../classFCommandFeature.html#a3ff8a0398db30ce422496f2281dd3b60',1,'FCommandFeature']]],
+  ['onopenscreenshotdir_1',['OnOpenScreenShotDir',['../classFScreenshotFeature.html#af01017cd01130c08bf1c63309ea1e2a6',1,'FScreenshotFeature']]],
+  ['onselectedmap_2',['OnSelectedMap',['../classFLevelFeature.html#ac20da5828b1adc7686fbd96ba8abd1c8',1,'FLevelFeature']]],
+  ['onselectedmap_5fopen_3',['OnSelectedMap_Open',['../classFLevelFeature.html#a536602356f4e9dcdfcfbe85094782bdb',1,'FLevelFeature']]],
+  ['onselectedmap_5fplayineditor_4',['OnSelectedMap_PlayInEditor',['../classFLevelFeature.html#a31e8a2482aafbc4082a996e3a5a7a0d9',1,'FLevelFeature']]],
+  ['onselectedmap_5fplayselectedviewport_5',['OnSelectedMap_PlaySelectedViewport',['../classFLevelFeature.html#ad659ce46cd4d37948b219e9aa104c2b3',1,'FLevelFeature']]]
 ];
