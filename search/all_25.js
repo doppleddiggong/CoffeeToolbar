eafc92f9a2b375b9273af31575477e4e7171ab4d
doppleddiggong <doppleddiggong@users.noreply.github.com>
2025-10-06 09:19:37 +0000

    deploy: c90445483fde1641fa36b47475ab87ad1f127da0

--- search/all_25.js
+++ search/all_25.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['제공_20기능_0',['제공 기능',['../index.html#autotoc_md13',1,'']]]
+  ['제공_20기능_0',['제공 기능',['../index.html#autotoc_md13',1,'']]],
+  ['제작자_20author_1',['제작자 (Author)',['../index.html#autotoc_md17',1,'']]]
 ];
--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['adding_20custom_20commands_0',['3. 커스텀 명령어 추가 (Adding Custom Commands)',['../index.html#autotoc_md10',1,'']]],
-  ['author_1',['Author',['../index.html#autotoc_md17',1,'']]]
+  ['author_1',['제작자 (Author)',['../index.html#autotoc_md17',1,'']]]
 ];
